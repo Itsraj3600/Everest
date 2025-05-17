484ba9ef70904b99755af2c58f7e06a31dbb2f0e
Rajkirat Singh <93331120+Itsraj3600@users.noreply.github.com>
2025-05-17 00:02:05 -0400

    modified:   Users/Sanish/id.html 	modified:   Users/Sneha/Photo.png 	new file:   Users/Sneha/barcode.PNG 	modified:   Users/Sneha/id.html 	modified:   Users/Sneha/index.html 	modified:   Users/Sneha/java/app.js 	modified:   Users/Sneha/java/id.js

--- Users/Sneha/java/id.js
+++ Users/Sneha/java/id.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", function () {
   const backButton = document.getElementById("customBackButton");
   backButton.addEventListener("click", function () {
-    window.location.href = "https://everestidentity.xyz/users/raj/index.html"; 
+    window.location.href = "https://everestidentity.xyz/users/sneha/index.html";
   });
 });
