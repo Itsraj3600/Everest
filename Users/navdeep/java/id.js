9ff0735b2f6e664242ef0c21e8f8002ba4a6c65b
Rajkirat Singh <itsraj3600@gmail.com>
2025-07-06 01:18:03 -0400

    Remove unused files and update links in list.html for navigation

--- Users/navdeep/java/id.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-  const backButton = document.getElementById("customBackButton");
-  backButton.addEventListener("click", function () {
-    window.location.href = "https://everestidentity.xyz/users/navdeep/index.html";
-  });
-});
